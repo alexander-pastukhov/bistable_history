f370017401b2b14aeb6e22cf994a64afa2782bb4
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2021-12-30 22:13:35 +0100

    Linting

--- R/fit_cumhist.R
+++ R/fit_cumhist.R
@@ -92,7 +92,7 @@
 #' @examples
 #' \donttest{
 #' data(br_singleblock)
-#' gamma_fit <- fit_cumhist(br_singleblock, state="State", duration="Duration")
+#' gamma_fit <- fit_cumhist(br_singleblock, state = "State", duration = "Duration")
 #' }
 fit_cumhist <- function(data,
                         state,
